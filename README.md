f2681516a650dd7522ccb4c93b32d3e225df3fdd
Thierry Lelegard <thierry@lelegard.fr>
2025-09-14 22:36:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,75 @@
+## Windows process memory mapping
+
+This sample program displays the memory mapping of modules in the process.
+
+The address of some symbols are displayed, along with the module into which
+there are defined.
+
+Problem: Some symbols from BCrypt or SSPI (SChannel) libraries have their
+address in the main executable when in debug mode, but not in release mode.
+Why?
+
+Sample output in release mode:
+~~~
+====== Modules (15) ======
+0x545F0000-0x545FBFFF : D:\win-memory-map\build\Release-x64\win-memory-map.exe
+0x86260000-0x862E8FFF : C:\WINDOWS\SYSTEM32\MSVCP140.dll
+0xAFD70000-0xAFD8DFFF : C:\WINDOWS\SYSTEM32\VCRUNTIME140.dll
+0xAFE50000-0xAFE5BFFF : C:\WINDOWS\SYSTEM32\VCRUNTIME140_1.dll
+0xB7190000-0xB7418FFF : C:\WINDOWS\SYSTEM32\WININET.dll
+0xC21D0000-0xC226DFFF : C:\WINDOWS\SYSTEM32\apphelp.dll
+0xC2ED0000-0xC2EDCFFF : C:\WINDOWS\SYSTEM32\Secur32.dll
+0xC51D0000-0xC5218FFF : C:\WINDOWS\SYSTEM32\SSPICLI.DLL
+0xC6040000-0xC6065FFF : C:\WINDOWS\SYSTEM32\bcrypt.dll
+0xC6550000-0xC669AFFF : C:\WINDOWS\System32\ucrtbase.dll
+0xC6780000-0xC6B72FFF : C:\WINDOWS\System32\KERNELBASE.dll
+0xC6B80000-0xC6CF6FFF : C:\WINDOWS\System32\CRYPT32.dll
+0xC7AB0000-0xC7B78FFF : C:\WINDOWS\System32\KERNEL32.DLL
+0xC7ED0000-0xC7FE7FFF : C:\WINDOWS\System32\RPCRT4.dll
+0xC8FC0000-0xC9228FFF : C:\WINDOWS\SYSTEM32\ntdll.dll
+
+====== Some symbols ======
+0x545F1A90 : wmain -> D:\win-memory-map\build\Release-x64\win-memory-map.exe
+0xB71C0C70 : InternetOpenW -> C:\WINDOWS\SYSTEM32\WININET.dll
+0xC51D1C00 : ApplyControlToken -> C:\WINDOWS\SYSTEM32\SSPICLI.DLL
+0xC51D1FE0 : InitializeSecurityContextW -> C:\WINDOWS\SYSTEM32\SSPICLI.DLL
+0xC51D2D70 : EncryptMessage -> C:\WINDOWS\SYSTEM32\SSPICLI.DLL
+0xC604B200 : BCryptEncrypt -> C:\WINDOWS\SYSTEM32\bcrypt.dll
+0xC604F590 : BCryptImportKey -> C:\WINDOWS\SYSTEM32\bcrypt.dll
+0xC6BDC5F0 : CertOpenStore -> C:\WINDOWS\System32\CRYPT32.dll
+0xC7AD4970 : GetCurrentProcess -> C:\WINDOWS\System32\KERNEL32.DLL
+~~~
+
+Sample output in debug mode:
+~~~
+====== Modules (15) ======
+0x282B0000-0x284B3FFF : C:\WINDOWS\SYSTEM32\ucrtbased.dll
+0x69670000-0x69750FFF : C:\WINDOWS\SYSTEM32\MSVCP140D.dll
+0x848D0000-0x84909FFF : D:\shared\win-memory-map\build\Debug-x64\win-memory-map.exe
+0xB7190000-0xB7418FFF : C:\WINDOWS\SYSTEM32\WININET.dll
+0xB7BA0000-0xB7BCFFFF : C:\WINDOWS\SYSTEM32\VCRUNTIME140D.dll
+0xC0170000-0xC017EFFF : C:\WINDOWS\SYSTEM32\VCRUNTIME140_1D.dll
+0xC2ED0000-0xC2EDCFFF : C:\WINDOWS\SYSTEM32\Secur32.dll
+0xC51D0000-0xC5218FFF : C:\WINDOWS\SYSTEM32\SSPICLI.DLL
+0xC6040000-0xC6065FFF : C:\WINDOWS\SYSTEM32\bcrypt.dll
+0xC6550000-0xC669AFFF : C:\WINDOWS\System32\ucrtbase.dll
+0xC6780000-0xC6B72FFF : C:\WINDOWS\System32\KERNELBASE.dll
+0xC6B80000-0xC6CF6FFF : C:\WINDOWS\System32\CRYPT32.dll
+0xC7AB0000-0xC7B78FFF : C:\WINDOWS\System32\KERNEL32.DLL
+0xC7ED0000-0xC7FE7FFF : C:\WINDOWS\System32\RPCRT4.dll
+0xC8FC0000-0xC9228FFF : C:\WINDOWS\SYSTEM32\ntdll.dll
+
+====== Some symbols ======
+0x848E1055 : wmain -> D:\shared\win-memory-map\build\Debug-x64\win-memory-map.exe
+0x848E17A3 : EncryptMessage -> D:\shared\win-memory-map\build\Debug-x64\win-memory-map.exe
+0x848E17EE : BCryptEncrypt -> D:\shared\win-memory-map\build\Debug-x64\win-memory-map.exe
+0x848E1947 : BCryptImportKey -> D:\shared\win-memory-map\build\Debug-x64\win-memory-map.exe
+0xB71C0C70 : InternetOpenW -> C:\WINDOWS\SYSTEM32\WININET.dll
+0xC51D1C00 : ApplyControlToken -> C:\WINDOWS\SYSTEM32\SSPICLI.DLL
+0xC51D1FE0 : InitializeSecurityContextW -> C:\WINDOWS\SYSTEM32\SSPICLI.DLL
+0xC6BDC5F0 : CertOpenStore -> C:\WINDOWS\System32\CRYPT32.dll
+0xC7AD4970 : GetCurrentProcess -> C:\WINDOWS\System32\KERNEL32.DLL
+~~~
+
+Because of ASLR, the addresses are different in each execution. However,
+the symbols are always located in the same module.
---
+++ win-memory-map.cpp
@@ -0,0 +1,169 @@
+//----------------------------------------------------------------------------
+// win-memory-map - Copyright (c) 2025, Thierry Lelegard
+// BSD 2-Clause License, see LICENSE file.
+//----------------------------------------------------------------------------
+
+#define SECURITY_WIN32 1           // used by sspi.h (versus SECURITY_KERNEL)
+#define SCHANNEL_USE_BLACKLISTS 1  // for SCH_CREDENTIALS
+
+#include <iostream>
+#include <utility>
+#include <cassert>
+#include <vector>
+#include <list>
+#include <string>
+#include <array>
+#include <cwctype>
+#include <iomanip>
+#include <algorithm>
+#include <windows.h>
+#include <strsafe.h>
+#include <psapi.h>
+#include <wininet.h>
+#include <subauth.h>
+#include <sspi.h>
+#include <schannel.h>
+#include <bcrypt.h>
+
+// Remove silly macros from VC headers.
+#if defined(min)
+    #undef min
+#endif
+#if defined(max)
+    #undef max
+#endif
+
+#pragma comment(lib, "wininet.lib")
+#pragma comment(lib, "crypt32.lib")
+#pragma comment(lib, "secur32.lib")
+#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "bcrypt.lib")
+
+
+//----------------------------------------------------------------------------
+// Printf-like format with wide characters.
+//----------------------------------------------------------------------------
+
+template <class... Args>
+std::wstring Format(const wchar_t* fmt, Args&&... args)
+{
+    std::array<wchar_t, 2048> buffer;
+    StringCbPrintfW(buffer.data(), buffer.size() * sizeof(buffer[0]), fmt, std::forward<Args>(args)...);
+    buffer[buffer.size() - 1] = 0;
+    return std::wstring(buffer.data());
+}
+
+
+//----------------------------------------------------------------------------
+// Get all modules in the process.
+//----------------------------------------------------------------------------
+
+void WinGetProcessModules(std::vector<HMODULE>& mods)
+{
+    DWORD retsize = 0;
+    mods.resize(512);
+    if (EnumProcessModules(GetCurrentProcess(), mods.data(), DWORD(mods.size() * sizeof(mods[0])), &retsize)) {
+        mods.resize(std::min<size_t>(mods.size(), retsize / sizeof(mods[0])));
+    }
+    else {
+        mods.clear();
+    }
+}
+
+
+//----------------------------------------------------------------------------
+// Get system error message.
+//----------------------------------------------------------------------------
+
+std::wstring WinMessage(DWORD code = GetLastError())
+{
+    std::array<wchar_t, 2048> message;
+    DWORD length = FormatMessageW(FORMAT_MESSAGE_FROM_SYSTEM, nullptr, code, 0, message.data(), DWORD(message.size()), nullptr);
+
+    // If message is empty, try all loaded modules in the process.
+    if (length <= 0) {
+        // Get a list of handles for all loaded modules. These handles shall not be closed here.
+        std::vector<HMODULE> hmods;
+        WinGetProcessModules(hmods);
+        // Try all all modules, one by one, until a non-empty message is returned.
+        for (size_t i = 0; length <= 0 && i < hmods.size(); ++i) {
+            length = FormatMessageW(FORMAT_MESSAGE_FROM_HMODULE, hmods[i], code, 0, message.data(), DWORD(message.size()), nullptr);
+        }
+    }
+
+    return length > 0 ? std::wstring(message.data()) : Format(L"Error code: 0x%08X", code);
+}
+
+
+//----------------------------------------------------------------------------
+// Get a module file name.
+//----------------------------------------------------------------------------
+
+std::wstring WinModuleName(HMODULE hmod)
+{
+    if (hmod == nullptr || hmod == INVALID_HANDLE_VALUE) {
+        return std::wstring();
+    }
+    else {
+        std::array<wchar_t, 2048> path;
+        const DWORD len = GetModuleFileNameW(hmod, path.data(), DWORD(path.size()));
+        return len > 0 ? std::wstring(path.data(), size_t(len)) : WinMessage();
+    }
+}
+
+
+//----------------------------------------------------------------------------
+// Get the handle of the module containing an address.
+//----------------------------------------------------------------------------
+
+HMODULE WinModuleFromAddress(const void* addr)
+{
+    HMODULE hmod = nullptr;
+    static constexpr DWORD flags = GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT;
+    return GetModuleHandleExW(flags, reinterpret_cast<LPCWSTR>(addr), &hmod) ? hmod : nullptr;
+}
+
+
+//----------------------------------------------------------------------------
+// Application entry point.
+//----------------------------------------------------------------------------
+
+int wmain(int argc, wchar_t* argv[], wchar_t* envp[])
+{
+    // Build list of modules.
+    std::vector<HMODULE> hmods;
+    WinGetProcessModules(hmods);
+    std::list<std::wstring> lines;
+    for (auto h : hmods) {
+        MODULEINFO info = { nullptr, 0, nullptr };
+        GetModuleInformation(GetCurrentProcess(), h, &info, DWORD(sizeof(info)));
+        lines.push_back(Format(L"0x%08X-0x%08X : ", uintptr_t(info.lpBaseOfDll), uintptr_t(info.lpBaseOfDll) + info.SizeOfImage - 1) + WinModuleName(h));
+    }
+    lines.sort();
+
+    std::wcout << L"====== Modules (" << hmods.size() << L") ======" << std::endl;
+    for (const auto& l : lines) {
+        std::wcout << l << std::endl;
+    }
+
+    // Build list of symbols.
+    lines.clear();
+#define _ADD(symbol) lines.push_back(Format(L"0x%08X : %s -> ", uintptr_t(symbol), L#symbol) + WinModuleName(WinModuleFromAddress(symbol)))
+    _ADD(wmain);
+    _ADD(GetCurrentProcess);
+    _ADD(InternetOpenW);
+    _ADD(CertOpenStore);
+    _ADD(InitializeSecurityContextW);
+    _ADD(ApplyControlToken);
+    _ADD(EncryptMessage);
+    _ADD(BCryptImportKey);
+    _ADD(BCryptEncrypt);
+#undef _ADD
+    lines.sort();
+
+    std::wcout << std::endl << L"====== Some symbols ======" << std::endl;
+    for (const auto& l : lines) {
+        std::wcout << l << std::endl;
+    }
+    std::wcout << std::endl;
+}
